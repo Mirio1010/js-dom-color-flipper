53606b9e489a4fb3c76c0a45507917a319af3a99
Mirio1010 <miguelangelortega2021@gmail.com>
2026-01-05 21:28:37 -0500

    Implement hex color flipper using Dom events

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+
+let btn = document.getElementById('btn');
+let color = document.querySelector('.color');
+let index = 0
+btn.addEventListener('click', function() {
+    if (index === colors.length) {
+        index = 0;
+    }
+    document.body.style.backgroundColor = colors[index];
+    color.textContent = colors.at(index);
+    
+    
+    index++;
+    
+})
+
+function getRandomNumber() {
+    
+    return Math.random()
+}
+
+
+
+
